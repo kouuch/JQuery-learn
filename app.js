f2626f3ad06327a85481b0f158b283680521c11d
kouuch <rifkyirsada2@gmail.com>
2025-07-28 00:04:34 +0800

    get dan manipulation

--- app.js
+++ app.js
@@ -6,6 +6,22 @@
 
 
 $(document).ready(function () {
+    let price = $('<li>From $100.99</li>')
+    let price1 = $('<li>From $99.99</li>')
+    let price2 = $('<li>From $98.99</li>')
+
+    $('.vacation').before(price)//manipulasi dom before di atas after dibawh atau setelah li atau di atas li pertama
+    $('.vacation').prepend(price1)//manipulasi dom before di atas after dibawh atau setelah li atau di atas li pertama
+
+        price2  .appendTo($('.vacation'));//variabel price tolong tambahkan ke klas ,vacation sebelumnya atau di no10 kebalikan vacation menambhkan elemn pricenya == hasilnya di bawaah clas vacation
+
+    //get
+    console.log($('.akane').text())
+    console.log($('.akane').html())
+    console.log($('.input').val())
+
+    
+    //btn
     $('button').click(function(){
         $('p').toggle() //togle untuk menyembunyikan dan menampilkan
         $('li').text('Kokoroshi')//pas di klik maka akan li berubah jadi kokoroshi

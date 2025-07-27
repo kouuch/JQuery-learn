c9fb00003ff6846a4404d655b6c7e1176e216a1d
kouuch <rifkyirsada2@gmail.com>
2025-07-27 22:34:30 +0800

    fungsi click di jquery

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-    console.log('test')
-    $('li').text('Kokoroshi')
-    $('#up').text('Lily')
-})
+// $(document).ready(function(){
+//     console.log('test')
+//     $('li').text('Kokoroshi')
+//     $('#up').text('Lily')
+// })
+
+
+$(document).ready(function () {
+    $('button').click(function(){
+        $('p').toggle() //togle untuk menyembunyikan dan menampilkan
+        $('li').text('Kokoroshi')//pas di klik maka akan li berubah jadi kokoroshi
+    })
+});
